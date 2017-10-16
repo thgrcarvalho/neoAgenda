cc49942340fb0e9b5a5fbfa9f53b3d08a988ea70
f11640208704 <f11640208704@lafe-sed-adm-20.echo.local>
2017-10-16 11:28:44 -0200

    Configurações iniciais do projeto
    -Adicionado componente agenda
    -Adicionado o módulo do Fullcalendar

--- Agenda20/src/app/app.module.ts
+++ Agenda20/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgendaComponent } from './agenda/agenda.component';
+import { CalendarComponent } from 'ap-angular2-fullcalendar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgendaComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule
